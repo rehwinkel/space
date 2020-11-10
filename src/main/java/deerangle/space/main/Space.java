386e50f1532be3dec4a3c294f0601438dc102338
Ian Rehwinkel <ian.rehwinkel@tutanota.com>
2020-11-10 19:17:44 +0100

    Initial Commit

---
+++ src/main/java/deerangle/space/main/Space.java
@@ -0,0 +1,13 @@
+package deerangle.space.main;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(Space.MOD_ID)
+public class Space {
+
+    public static final String MOD_ID = "space";
+
+    public Space() {
+    }
+
+}
