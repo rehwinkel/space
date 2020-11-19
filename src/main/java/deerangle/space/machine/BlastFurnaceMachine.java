eee2693a116f813335f9a6e55cba67d9b54b80fd
Ian Rehwinkel <ian.rehwinkel@tutanota.com>
2020-11-19 22:59:07 +0100

    did some blast furnace processing code

--- src/main/java/deerangle/space/machine/BlastFurnaceMachine.java
+++ src/main/java/deerangle/space/machine/BlastFurnaceMachine.java
@@ -10,6 +10,7 @@ import deerangle.space.registry.MachineTypeRegistry;
 import deerangle.space.registry.RecipeRegistry;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.CompoundNBT;
+import net.minecraft.tileentity.AbstractFurnaceTileEntity;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraftforge.common.ForgeHooks;
@@ -26,6 +27,9 @@ public class BlastFurnaceMachine extends Machine {
 
     private int currentMaxBurnTime = 0;
     private int currentBurnTime = 0;
+    private int currentMaxProgress = 0;
+    private int currentProgress = 0;
+    private BlastFurnaceRecipe currentRecipe = null;
 
     public BlastFurnaceMachine() {
         super(MachineTypeRegistry.BLAST_FURNACE,
@@ -40,22 +44,63 @@ public class BlastFurnaceMachine extends Machine {
     @Override
     public void update(World world, BlockPos pos) {
         boolean wasBurning = this.isBurning();
-        Optional<BlastFurnaceRecipe> recipe = world.getRecipeManager().getRecipe(RecipeRegistry.BLAST_FURNACE_TYPE,
+        Optional<BlastFurnaceRecipe> recipeOpt = world.getRecipeManager().getRecipe(RecipeRegistry.BLAST_FURNACE_TYPE,
                 new StackInventory(this.input.getItemHandlerOrThrow().getStackInSlot(0)), world);
-        if (currentBurnTime == 0) {
+        if (currentBurnTime <= 1) {
+            if (currentBurnTime == 0) {
+                this.currentMaxProgress = 0;
+                this.currentProgress = 0;
+                this.currentRecipe = null;
+                this.progress.setProgress(0);
+            }
             ItemStack currentFuelStack = this.fuel.getItemHandlerOrThrow().getStackInSlot(0);
             int burnTime = ForgeHooks.getBurnTime(currentFuelStack);
-            if (burnTime > 0) {
+            if (burnTime > 0 && recipeOpt.isPresent()) {
                 this.fuel.getItemHandlerOrThrow().extractItem(0, 1, false);
                 currentMaxBurnTime = burnTime;
-                currentBurnTime = currentMaxBurnTime;
+                currentBurnTime += currentMaxBurnTime + 1;
             }
-        } else {
+        }
+
+        if (currentBurnTime > 0) {
+            if (currentRecipe == null) {
+                recipeOpt.ifPresent(recipe -> {
+                    this.currentRecipe = recipe;
+                    this.currentMaxProgress = recipe.getDuration();
+                    this.currentProgress = recipe.getDuration();
+                });
+            } else {
+                this.currentProgress--;
+
+                if (this.currentProgress == 0) {
+                    if (ItemStack.EMPTY == this.output.getItemHandlerOrThrow()
+                            .insertItem(0, this.currentRecipe.getCraftingResult(null), true)) {
+                        this.output.getItemHandlerOrThrow()
+                                .insertItem(0, this.currentRecipe.getCraftingResult(null), false);
+                        this.input.getItemHandlerOrThrow().extractItem(0, 1, false);
+                    }
+                    this.currentRecipe = null;
+                    this.currentMaxProgress = 0;
+                }
+            }
+        }
+
+        if (currentBurnTime > 0) {
             currentBurnTime--;
         }
+
+        if (currentMaxProgress > 0) {
+            this.progress.setProgress(1 - (currentProgress / (float) currentMaxProgress));
+        } else {
+            this.progress.setProgress(0);
+        }
+
         if (currentMaxBurnTime > 0) {
             this.burn.setProgress(currentBurnTime / (float) currentMaxBurnTime);
+        } else {
+            this.burn.setProgress(0);
         }
+
         if (wasBurning != this.isBurning()) {
             world.setBlockState(pos, world.getBlockState(pos).with(MachineBlock.RUNNING, this.isBurning()), 3);
         }
