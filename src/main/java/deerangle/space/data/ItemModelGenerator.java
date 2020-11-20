f4a6383162a4a01a998e2f5d86bfa14466bf6510
Igli2 <sg.gerdon@gmx.de>
2020-11-20 17:07:17 +0100

    Added all items to resource registry and data gens

--- src/main/java/deerangle/space/data/ItemModelGenerator.java
+++ src/main/java/deerangle/space/data/ItemModelGenerator.java
@@ -21,6 +21,33 @@ public class ItemModelGenerator extends ItemModelProvider {
     @Override
     protected void registerModels() {
         addFlatItem(ResourceRegistry.STEEL_INGOT.get());
+        addFlatItem(ResourceRegistry.COPPER_INGOT.get());
+        addFlatItem(ResourceRegistry.ALUMINIUM_INGOT.get());
+        addFlatItem(ResourceRegistry.TITANIUM_INGOT.get());
+        addFlatItem(ResourceRegistry.STEEL_NUGGET.get());
+        addFlatItem(ResourceRegistry.COPPER_NUGGET.get());
+        addFlatItem(ResourceRegistry.ALUMINIUM_NUGGET.get());
+        addFlatItem(ResourceRegistry.TITANIUM_NUGGET.get());
+        addFlatItem(ResourceRegistry.ALUMINIUM_PLATE.get());
+        addFlatItem(ResourceRegistry.COPPER_PLATE.get());
+        addFlatItem(ResourceRegistry.IRON_PLATE.get());
+        addFlatItem(ResourceRegistry.STEEL_PLATE.get());
+        addFlatItem(ResourceRegistry.BATTERY.get());
+        addFlatItem(ResourceRegistry.CYLINDER.get());
+        addFlatItem(ResourceRegistry.IGNITION_COIL.get());
+        addFlatItem(ResourceRegistry.INDUSTRIAL_PISTON.get());
+        addFlatItem(ResourceRegistry.COPPER_PIPE.get());
+        addFlatItem(ResourceRegistry.IRON_PIPE.get());
+        addFlatItem(ResourceRegistry.IRON_DUST.get());
+        addFlatItem(ResourceRegistry.QUARTZ_DUST.get());
+        addFlatItem(ResourceRegistry.STEEL_ROD.get());
+        addFlatItem(ResourceRegistry.HEAT_RESISTENT_GLASS.get());
+        addFlatItem(ResourceRegistry.MACHINE_BASE.get());
+        addFlatItem(ResourceRegistry.SILICA_TILE.get());
+        addFlatItem(ResourceRegistry.PLATED_ROCKET_BODY.get());
+        addFlatItem(ResourceRegistry.ROCKET_CONE.get());
+        addFlatItem(ResourceRegistry.ROCKET_FIN.get());
+        addFlatItem(ResourceRegistry.ROCKET_THRUSTER.get());
     }
 
 }
--- src/main/java/deerangle/space/registry/ResourceRegistry.java
+++ src/main/java/deerangle/space/registry/ResourceRegistry.java
@@ -28,8 +28,62 @@ public class ResourceRegistry extends AbstractRegistry {
             AbstractBlock.Properties.create(Material.ROCK).setRequiresTool().harvestLevel(1)
                     .hardnessAndResistance(3.0F, 3.0F)));
 
+    public static final RegistryObject<Item> ALUMINIUM_INGOT = ITEMS
+            .register("aluminium_ingot", () -> new Item(new Item.Properties().group(TAB)));
+    public static final RegistryObject<Item> COPPER_INGOT = ITEMS
+            .register("copper_ingot", () -> new Item(new Item.Properties().group(TAB)));
     public static final RegistryObject<Item> STEEL_INGOT = ITEMS
             .register("steel_ingot", () -> new Item(new Item.Properties().group(TAB)));
+    public static final RegistryObject<Item> TITANIUM_INGOT = ITEMS
+            .register("titanium_ingot", () -> new Item(new Item.Properties().group(TAB)));
+    public static final RegistryObject<Item> ALUMINIUM_NUGGET = ITEMS
+            .register("aluminium_nugget", () -> new Item(new Item.Properties().group(TAB)));
+    public static final RegistryObject<Item> COPPER_NUGGET = ITEMS
+            .register("copper_nugget", () -> new Item(new Item.Properties().group(TAB)));
+    public static final RegistryObject<Item> STEEL_NUGGET = ITEMS
+            .register("steel_nugget", () -> new Item(new Item.Properties().group(TAB)));
+    public static final RegistryObject<Item> TITANIUM_NUGGET = ITEMS
+            .register("titanium_nugget", () -> new Item(new Item.Properties().group(TAB)));
+    public static final RegistryObject<Item> ALUMINIUM_PLATE = ITEMS
+            .register("aluminium_plate", () -> new Item(new Item.Properties().group(TAB)));
+    public static final RegistryObject<Item> COPPER_PLATE = ITEMS
+            .register("copper_plate", () -> new Item(new Item.Properties().group(TAB)));
+    public static final RegistryObject<Item> IRON_PLATE = ITEMS
+            .register("iron_plate", () -> new Item(new Item.Properties().group(TAB)));
+    public static final RegistryObject<Item> STEEL_PLATE = ITEMS
+            .register("steel_plate", () -> new Item(new Item.Properties().group(TAB)));
+    public static final RegistryObject<Item> BATTERY = ITEMS
+            .register("battery", () -> new Item(new Item.Properties().group(TAB)));
+    public static final RegistryObject<Item> CYLINDER = ITEMS
+            .register("cylinder", () -> new Item(new Item.Properties().group(TAB)));
+    public static final RegistryObject<Item> IGNITION_COIL = ITEMS
+            .register("ignition_coil", () -> new Item(new Item.Properties().group(TAB)));
+    public static final RegistryObject<Item> INDUSTRIAL_PISTON = ITEMS
+            .register("industrial_piston", () -> new Item(new Item.Properties().group(TAB)));
+    public static final RegistryObject<Item> COPPER_PIPE = ITEMS
+            .register("copper_pipe", () -> new Item(new Item.Properties().group(TAB)));
+    public static final RegistryObject<Item> IRON_PIPE = ITEMS
+            .register("iron_pipe", () -> new Item(new Item.Properties().group(TAB)));
+    public static final RegistryObject<Item> IRON_DUST = ITEMS
+            .register("iron_dust", () -> new Item(new Item.Properties().group(TAB)));
+    public static final RegistryObject<Item> QUARTZ_DUST = ITEMS
+            .register("quartz_dust", () -> new Item(new Item.Properties().group(TAB)));
+    public static final RegistryObject<Item> STEEL_ROD = ITEMS
+            .register("steel_rod", () -> new Item(new Item.Properties().group(TAB)));
+    public static final RegistryObject<Item> HEAT_RESISTENT_GLASS = ITEMS
+            .register("heat_resistent_glass", () -> new Item(new Item.Properties().group(TAB)));
+    public static final RegistryObject<Item> MACHINE_BASE = ITEMS
+            .register("machine_base", () -> new Item(new Item.Properties().group(TAB)));
+    public static final RegistryObject<Item> SILICA_TILE = ITEMS
+            .register("silica_tile", () -> new Item(new Item.Properties().group(TAB)));
+    public static final RegistryObject<Item> PLATED_ROCKET_BODY = ITEMS
+            .register("plated_rocket_body", () -> new Item(new Item.Properties().group(TAB)));
+    public static final RegistryObject<Item> ROCKET_CONE = ITEMS
+            .register("rocket_cone", () -> new Item(new Item.Properties().group(TAB)));
+    public static final RegistryObject<Item> ROCKET_FIN = ITEMS
+            .register("rocket_fin", () -> new Item(new Item.Properties().group(TAB)));
+    public static final RegistryObject<Item> ROCKET_THRUSTER = ITEMS
+            .register("rocket_thruster", () -> new Item(new Item.Properties().group(TAB)));
 
     static {
         ITEMS.register("copper_ore", () -> new BlockItem(COPPER_ORE.get(), new Item.Properties().group(TAB)));
